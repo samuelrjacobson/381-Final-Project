d6e30e33d22b2cb06c9983c8a3d1e834a23f1450
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-12-15 06:02:04 +0000

    fix: Added ability to search for movie review vids
    Added youtube.py with method to search with YouTube API for videos of reviews/commentary for specified movie title
    Added fastapi get method to main.py allowing user to enter movie name and receive list of 10 youtube videos' information
    Added test_youtube.py and method to test_main.py to test new methods mentioned above
    Updated README.md and requirements.txt
    Changed to current branch in test.yml
    
    issue #2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from youtube import reviews_search
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 async def hello_world():
     return {"status": "Hello, world"}
+
+@app.get("/moviereviews/{movie_name}", response_model=List[dict])
+async def get_video_list(movie_name: str):
+    try:
+        return reviews_search(movie_name)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- test_main.py
+++ test_main.py
@@ -9,3 +9,17 @@ def test_hello_world():
     assert response.json() == {
         "status": "Hello, world"
     }
+
+def test_get_video_list():
+    response = client.get("/moviereviews/Harry_Potter")
+    assert response.status_code == 200
+    reviews_list = response.json()
+
+    assert len(reviews_list) == 10  
+    
+    for review in reviews_list:
+        assert not review['id'] == None
+        assert not review['title'] == None
+        assert not review['description'] == None
+
+    assert "Harry Potter".upper() in str(reviews_list).upper()
